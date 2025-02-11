5b163529268fa3eb3b067c3fa09d64e4bc51dc5b
luiza <luizabarbosa2087@gmail.com>
2025-02-11 19:03:16 -0300

    add :fire:

--- Projetocsharp.Common/obj/Debug/net8.0/Projetocsharp.Common.AssemblyInfo.cs
+++ Projetocsharp.Common/obj/Debug/net8.0/Projetocsharp.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projetocsharp.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886485f3b2149a751bba6a725f2558552a3d02af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projetocsharp.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projetocsharp.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
